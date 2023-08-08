4768068ae2754f874ac415b34bf3c001026e410a
Parisafrhni94 <parisafarahanicsc@gmail.com>
2023-08-07 21:31:50 -0400

    ..

--- part2/studio/src/App.js
+++ part2/studio/src/App.js
@@ -1,3 +1,4 @@
+//import React from 'react';
 import "./App.css";
 import RateARecipe from "./components/RateARecipe";
 import Button from "./components/Button";
@@ -5,13 +6,17 @@ import AuthorInfo from "./components/AuthorInfo";
 import IngredientList from "./components/Ingredients";
 import RecipeImage from "./components/RecipeImage";
 import RecipeName from "./components/RecipeName";
+import SaveButton from "./components/SaveButton";
+import ClickedButton from "./components/ClickedButton";
 
 export default function App() {
   return (
     <>
-      <RecipeImage />
       <RecipeName />
       <RateARecipe rating={4} />
+      <RecipeImage />
+      {/* <SaveButton /> */}
+      {/* <ClickedButton /> */}
       <Button saveButton={true} />
       <AuthorInfo />
       <IngredientList />
--- part2/studio/src/components/Ingredients.js
+++ part2/studio/src/components/Ingredients.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import recipedata from "./recipe.json";
 function IngredientList() {
-   return;
- }
- 
- export default IngredientList;
- 
-//import json file for the data
-//use a nested map to get inside the inner array
- 
+  return (
+    <div>
+      <h3>Ingredients</h3>
+      {recipedata.map((data) => (
+        <div key={data.name}>
+          {data.ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default IngredientList;
